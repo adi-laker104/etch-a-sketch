79414cffc0d833f81ffb82fe4b74e9b342fe0d20
Adi-laker104 <6759685+adi-laker104@users.noreply.github.com>
2024-08-28 17:36:40 -0700

    Add button to user prompt number of boxesand make a createGrid function which creates display of prompted boxes

--- js-file.js
+++ js-file.js
@@ -2,9 +2,60 @@
 const container = document.querySelector("#container");
 
 //create a 16 x 16 grid of square dives within container
-for (i =0; i < 16; i++) {
-    for (j = 0; j < 16; j++) {
-        let newDiv = document.createElement("div");
-        container.appendChild(newDiv);   
+function createGrid(numberOfSquares) {
+    for (i = 0; i < numberOfSquares; i++) {
+        xDiv = document.createElement("div");
+        container.appendChild(xDiv); 
+        for (j = 0; j < numberOfSquares; j++) {
+            yDiv = document.createElement("div"); 
+            yDiv.classList.add("grid");
+            xDiv.appendChild(yDiv); 
+        }
     }
 }
+
+
+// const btn = document.querySelector("button");
+// btn.addEventListener("click", () => {
+//     let numberOfSquares = prompt("Enter number of squares between 1 and 100");
+//     while (numberOfSquares > 100 || numberOfSquares < 1 ) {
+//         numberOfSquares = prompt("Enter number of squares between 1 and 100");
+//     }
+  
+//     createGrid(numberOfSquares);
+// });
+
+
+
+     
+     const btn = document.querySelector("button");
+     btn.addEventListener("click", () => {
+        let numberOfSquares = prompt("Enter number of squares between 1 and 100");
+            while (numberOfSquares > 100 || numberOfSquares < 1 ) {
+         numberOfSquares = prompt("Enter number of squares between 1 and 100");
+     }
+        createGrid(numberOfSquares);
+        let gridElements = document.querySelectorAll(".grid");
+        console.log(gridElements.length);
+        gridElements.forEach((box) =>  {
+            box.addEventListener("mouseover", () => {
+                box.style.backgroundColor = "black";    
+            });
+    });
+});
+
+
+
+
+ // If this logs 0, no elements with the "grid" class are found
+
+
+// console.log("YES")
+// gridElements.forEach((box) =>  {
+//     console.log("INLOOP")
+// box.addEventListener("mouseover", () => {
+//     console.log("SHOW");
+//     box.style.backgroundColor = "black";
+// });
+    
+// });
